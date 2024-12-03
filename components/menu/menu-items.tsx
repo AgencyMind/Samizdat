91516d714740225154ea1d94c5759133bd11b153
jk <jk@skybound.xyz>
2024-12-03 12:06:17 -0500

    socials

--- components/menu/menu-items.tsx
+++ components/menu/menu-items.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 
-const firstLine = [
-  "stochastic blue",
-  "invoke agents",
-  "quickist"
+interface MenuItem {
+  text: string;
+  href: string;
+}
+
+const firstLine: MenuItem[] = [
+  { text: "stochastic blue", href: "https://stochasticblue.com/" },
+  { text: "invoke agents", href: "#" },
+  { text: "quickist", href: "https://quickist.pro/" }
 ];
 
-const secondLine = [
-  "lens",
-  "farcaster",
-  "bluesky",
-  "threads"
+const secondLine: MenuItem[] = [
+  { text: "lens", href: "https://orb.club/@isekai" },
+  { text: "farcaster", href: "https://warpcast.com/samizdat" },
+  { text: "bluesky", href: "https://bsky.app/profile/samizdat.ink" },
+  { text: "threads", href: "https://www.threads.net/@kende" }
 ];
 
 export function MenuItems() {
-  const renderLine = (items: string[]) => (
+  const renderLine = (items: MenuItem[]) => (
     <div className="flex items-center justify-center font-mono">
       {items.map((item, index) => (
-        <React.Fragment key={item}>
-          <span className="text-blue-500 hover:underline cursor-pointer">
-            {item}
-          </span>
+        <React.Fragment key={item.text}>
+          <a 
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            {item.text}
+          </a>
           {index < items.length - 1 && (
             <span className="text-blue-500 mx-3">*</span>
           )}
